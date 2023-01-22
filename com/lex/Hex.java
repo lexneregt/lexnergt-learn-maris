5612e00cad4a8ee13cc339e018adff135ad02391
lexnergt <lexneregt@gmail.com>
2023-01-22 15:32:45 +0800

    substring learn

---
+++ com/lex/Hex.java
@@ -0,0 +1,9 @@
+package com.lex;
+
+public class Hex {
+    private static String s = "hello World";
+
+    public static void main(String[] args) {
+        System.out.println(s.substring(1));
+    }
+}
